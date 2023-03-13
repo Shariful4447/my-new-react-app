cbe1656bd7c9d7d8e7a30a5a2ad0935da4f7db83
Shariful4447 <shariful.islam4447@gmail.com>
2023-03-13 15:02:22 +0600

    Started New App

---
+++ my-new-react-app/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>{2+4}</h1>
+        <p>New App startet Happy Learning 2+5 </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
