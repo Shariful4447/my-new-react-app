d7b079b032b5c75bb840d312468459d8139dc5ef
Shariful4447 <shariful.islam4447@gmail.com>
2023-03-17 17:47:32 +0100

    dynamic component with style

--- my-new-react-app/src/App.js
+++ my-new-react-app/src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -36,35 +36,49 @@ function App() {
         <Person name={nayoks[0]} Nayika="Moushumi"></Person>
         <Person name="Jasim Nayok" Nayika="Moyori"></Person>
         <Person name="Umar sani" Nayika="Dilruba khan"></Person>
-        
+         */}
+
+         <p>I am React person</p>
+         <Person name="Munna Bhai" job="Football khele"></Person>
+         <Person name="Masum" job="Khela dekhe"></Person>
       </header>
     </div>
   );
 }
 
-function Person(props){
-  // const personStyle={
-  //   border:'2px solid red',
-  //   margin:'10px'
-  // }
-  return(
-  <div style={{border:'2px solid yellow',margin:'10px'}}>
-  <h1>Nayok : {props.name} </h1>
-  <h3>Hero of :{props.Nayika} </h3>
-  </div>)
-  
-}
-// function Person2(){
-//   const personStyle={
-//     border:'2px solid red',
-//     margin:'10px'
-//   }
+// function Person(props){
+//   // const personStyle={
+//   //   border:'2px solid red',
+//   //   margin:'10px'
+//   // }
 //   return(
-//   <div style={personStyle}>
-//   <h1>Name : sakib khan </h1>
-//   <h3>Hero of the year</h3>
+//   <div style={{border:'2px solid yellow',margin:'10px'}}>
+//   <h1>Nayok : {props.name} </h1>
+//   <h3>Hero of :{props.Nayika} </h3>
 //   </div>)
   
 // }
+// // function Person2(){
+// //   const personStyle={
+// //     border:'2px solid red',
+// //     margin:'10px'
+// //   }
+// //   return(
+// //   <div style={personStyle}>
+// //   <h1>Name : sakib khan </h1>
+// //   <h3>Hero of the year</h3>
+// //   </div>)
+  
+// // }
+
+
+function Person(props){
+  return(
+  <div style={{border:'2px solid yellow',width:'400px'}}>
+    <h3>My Name : {props.name}</h3>
+    <p>My Profession: {props.job} </p>
+
+  </div>)
+}
 
 export default App;
