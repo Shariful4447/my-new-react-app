ad703c08a5f35011d9b014b11fe84a0d8c486a21
Shariful4447 <shariful.islam4447@gmail.com>
2023-03-17 17:22:01 +0100

    component started

--- my-new-react-app/src/App.js
+++ my-new-react-app/src/App.js
@@ -30,9 +30,39 @@ function App() {
         >
           Learn React
         </a>
+        <h1>new Name</h1>
+        <Person></Person>
+        <Person2></Person2>
+        <Person></Person>
+        
       </header>
     </div>
   );
+}
+
+function Person(){
+  // const personStyle={
+  //   border:'2px solid red',
+  //   margin:'10px'
+  // }
+  return(
+  <div style={{border:'2px solid yellow',margin:'10px'}}>
+  <h1>Name : sakib khan </h1>
+  <h3>Hero of the year</h3>
+  </div>)
+  
+}
+function Person2(){
+  const personStyle={
+    border:'2px solid red',
+    margin:'10px'
+  }
+  return(
+  <div style={personStyle}>
+  <h1>Name : sakib khan </h1>
+  <h3>Hero of the year</h3>
+  </div>)
+  
 }
 
 export default App;
